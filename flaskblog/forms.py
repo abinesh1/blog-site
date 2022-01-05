98ce5060082ce4fe0b02c7c03fa6f2c198238dc6
Abinesh Sankar <abinesh1@outlook.com>
2022-01-05 18:00:29 -0500

    register/login

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -11,8 +11,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators = [DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError('Username taken. Choose another')
+        
+    def validate_email(self, email):
+        email = User.query.filter_by(email = email.data).first()
+        if email:
+            raise ValidationError('Account exists. Please login or use another email id')
+
 class LoginForm(FlaskForm):
     email = StringField('Email', validators = [DataRequired(), Email()])
     password = PasswordField('Password', validators = [DataRequired()])
     remember = BooleanField('Remember me')
-    submit = SubmitField('Sign up') 
+    submit = SubmitField('Login') 
